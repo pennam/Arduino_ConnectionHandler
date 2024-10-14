af6f8af8e9c7a0a82cd9aae79be1e42697a86b6b
pennam <m.pennasilico@arduino.cc>
2024-10-14 10:38:37 +0200

    catM1: reset internal state machine

--- src/CatM1ConnectionHandler.cpp
+++ src/CatM1ConnectionHandler.cpp
@@ -91,6 +91,7 @@ NetworkConnectionState CatM1ConnectionHandler::update_handleDisconnecting()
 
 NetworkConnectionState CatM1ConnectionHandler::update_handleDisconnected()
 {
+  GSM.end();
   if (_keep_alive)
   {
     return NetworkConnectionState::INIT;
