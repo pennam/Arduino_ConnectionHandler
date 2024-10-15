8dcb05da2622e284013250f2a54733a2c58b3e16
pennam <m.pennasilico@arduino.cc>
2024-10-15 11:33:11 +0200

    catM1: do not rely on time stored in the modem

--- src/CatM1ConnectionHandler.cpp
+++ src/CatM1ConnectionHandler.cpp
@@ -46,7 +46,10 @@ CatM1ConnectionHandler::CatM1ConnectionHandler(const char * pin, const char * ap
 
 unsigned long CatM1ConnectionHandler::getTime()
 {
-  return GSM.getTime();
+  /* It is not safe to call GSM.getTime() since we don't know if modem internal
+   * RTC is in sync with current time.
+   */
+  return 0;
 }
 
 /******************************************************************************
